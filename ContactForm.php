d846cb1d5c2733890701183823b77d0dafcafec8
bi-it-mem <martim.menalopes@espas.ch>
2024-11-01 17:00:07 +0100

    File fixes und ContactForm added

--- ContactForm.php
+++ ContactForm.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { /* function that tells the server to
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
  
-    $to = "martim.mena.251@gmail.com";/* this part shows where the mail goes and how the incoming mail looks upon reception*/
+    $to = "martim.menalopes@espas.ch";/* this part shows where the mail goes and how the incoming mail looks upon reception*/
     $subject = "You've got new Mail from $name";
     $body = "Name: $name\n";
     $body .= "Email: $email\n\n";
